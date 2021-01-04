670347850750d0b7f639eac27559878bfd2f89da
Ahmed Habib <ahmedboboooooko@gmail.com>
2021-01-04 20:47:38 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menuToggle =document.querySelector('.toggle');
+    menuToggle.classList.toggle('active');
+   const banner =document.querySelector('.banner');
+    banner.classList.toggle('active');
+}
